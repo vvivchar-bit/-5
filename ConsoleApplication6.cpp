3c2f6785ee56be1c842e5fdb5198b00b30a34767
vvivchar-bit <v.vivchar@istu.edu.ua>
2026-03-22 13:26:18 +0200

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+class SortArray
+{
+private:
+    T arr[100];
+    int size;
+
+public:
+    SortArray(int s)
+    {
+        size = s;
+    }
+
+    void Input()
+    {
+        cout << "Enter " << size << " elements:" << endl;
+        for (int i = 0; i < size; i++)
+        {
+            cout << "arr[" << i << "] = ";
+            cin >> arr[i];
+        }
+    }
+
+    void Sort()
+    {
+        for (int i = 0; i < size - 1; i++)
+        {
+            for (int j = 0; j < size - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    void Show()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    int n = 5;
+
+    cout << "Integer array:" << endl;
+    SortArray<int> intArray(n);
+    intArray.Input();
+    intArray.Sort();
+    cout << "Sorted integer array: ";
+    intArray.Show();
+
+    cout << endl;
+
+    cout << "Double array:" << endl;
+    SortArray<double> doubleArray(n);
+    doubleArray.Input();
+    doubleArray.Sort();
+    cout << "Sorted double array: ";
+    doubleArray.Show();
+
+    cout << endl;
+
+    cout << "Char array:" << endl;
+    SortArray<char> charArray(n);
+    charArray.Input();
+    charArray.Sort();
+    cout << "Sorted char array: ";
+    charArray.Show();
+
+    return 0;
+}
---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+void BubbleSort(T arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                T temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+template <class T>
+void ShowArray(T arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int a[5] = { 5, 2, 8, 1, 3 };
+    double b[5] = { 4.4, 1.1, 3.3, 0.5, 2.2 };
+    char c[5] = { 'd', 'a', 'e', 'b', 'c' };
+
+    cout << "Before sorting integers: ";
+    ShowArray(a, 5);
+    BubbleSort(a, 5);
+    cout << "After sorting integers: ";
+    ShowArray(a, 5);
+
+    cout << endl;
+
+    cout << "Before sorting doubles: ";
+    ShowArray(b, 5);
+    BubbleSort(b, 5);
+    cout << "After sorting doubles: ";
+    ShowArray(b, 5);
+
+    cout << endl;
+
+    cout << "Before sorting chars: ";
+    ShowArray(c, 5);
+    BubbleSort(c, 5);
+    cout << "After sorting chars: ";
+    ShowArray(c, 5);
+
+    return 0;
+}
